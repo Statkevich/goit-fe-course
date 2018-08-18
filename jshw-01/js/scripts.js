dcad3ed5c8b79187cd125dfc353917984f0190e5
Yevgeniya Statkevich <statkevich.y@gmail.com>
2018-08-18 20:51:04 +0300

    loaded new files

---
+++ jshw-01/js/scripts.js
@@ -0,0 +1,24 @@
+'use strict';
+const adminLogin = 'admin';
+const adminPassword = 'm4ngo1zh4ackz0r';
+const denied = 'Доступ запрещен!';
+const canceled = 'Отменено пользователем!';
+const welcome = 'Добро пожаловать!'; 
+
+const  userLogin = prompt ('Введите логин')
+console.log ('логин', userLogin);
+if (userLogin === null) {
+	alert (canceled);
+} else if (userLogin !== adminLogin) {
+	alert (denied); 
+} else ; {
+const  userPassword = prompt ('Введите пароль')
+console.log ('Пароль', userPassword)
+if (userPassword === null) {
+	alert (canceled); 
+} else if (userPassword !== adminPassword) {
+	alert (denied); 	
+} else if (userPassword === adminPassword) {
+    alert (welcome); 
+}
+}
